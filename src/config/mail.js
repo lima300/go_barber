d409ee920bb7edf00a2234264b20a83c5515af80
Otávio Lima <olima94@gmail.com>
2020-04-02 18:22:21 -0300

    :construction: Work in progress (Nodemailer)

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: '7a2995f9b349f3',
+        pass: '44e44e6f5c6728',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
